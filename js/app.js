cef121f952eef068326995fe9841831fe1ef3176
Matan Uchen <matan.uchen@gmail.com>
2016-08-28 16:41:57 -0400

    Basic functionality for one player done.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-var app = angular.module('app', []);
+var app = angular.module('lifeCounter', []);
 
 app.controller('MainController', function($scope) {
+    $scope.players = [];
+    $scope.players.push(new Player());
 
+    $scope.addPlayer = function() {
+        $scope.players.push(new Player());
+    }
+
+    function Player() {
+        this.life = 20;
+        this.add = function(n) {
+            this.life += n;
+        };
+        this.sub = function(n) {
+            this.life -= n;
+        };
+        this.reset = function(n) {
+            this.life = 20;
+        };
+    }
 });
